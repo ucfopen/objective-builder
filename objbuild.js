4d48fc936aadb62a66251cf6cc43333835f72ae7
Elise Heron <elise.heron93@gmail.com>
2014-05-27 15:25:25 -0400

    Began fixing navigation |30|

--- objbuild.js
+++ objbuild.js
@@ -1,13 +1,13 @@
 //Function to enable sliding via bottom bar
 var active_slide = 1;
-
+var slides = ["begin", "condition","audience","behavior","degree"];
 $(document).on("click", "#bottom_bar button", function() {
     var id = $(this).val();
     
-    $("#slide_" + active_slide).removeClass("active");
+    $("#" + slides[active_slide]).removeClass("active");
     
     active_slide = id;
     
-    $("#slide_" + id).addClass("active");
+    $("#" + slides[id]).addClass("active");
 });
 //Function to enable navigation between slides
